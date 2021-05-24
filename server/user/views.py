40e5bc637e7dee88e060c6244e3b2bf2c03ebbfd
Lucas Divino <divino@pop-os.localdomain>
2021-05-24 20:42:37 -0300

    add view

--- server/user/views.py
+++ server/user/views.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
 from user.serializer import UserSerializer
+from user.models import Lugar
 
 class CustomAuthToken(ObtainAuthToken):
 
@@ -25,3 +27,11 @@ class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class OptimalPath(APIView):
+    def get(self, request):
+        user_id = self.request.query_params.get('user_id')
+        lugares = Lugar.objects.filter(user_id=user_id)
+
+    
