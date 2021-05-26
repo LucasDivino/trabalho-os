717864632144f8855d1690586c89a77a5150ccd0
Lucas Divino <divino@pop-os.localdomain>
2021-05-25 22:30:30 -0300

    mensagem de commit bem troll pq o trabalho é troll

--- server/user/models.py
+++ server/user/models.py
@@ -7,6 +7,7 @@ class Lugar(models.Model):
     latitude =  models.DecimalField(blank=True, decimal_places=20, max_digits=30)
     longitude =  models.DecimalField(blank=True, decimal_places=20, max_digits=30)
     name = models.CharField(blank=True, max_length=30)
+    destino = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'lugares'
